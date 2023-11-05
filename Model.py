3622e67e7a47301801573edbd6647a4969f6c386
yurimaster2112 <104884614+yurimaster2112@users.noreply.github.com>
2023-11-05 00:07:53 -0400

    Model #1
    Thoughts and pseudo codes

---
+++ Model.py
@@ -0,0 +1,10 @@
+# Initial thought and codes to create model and define strategy
+
+# Import
+import numpy as np
+import pandas as pd
+
+
+# Historical data needs to be in a csv format
+# Load historical stock data
+data = pd.read_csv('path_to_data.csv')
