8ca1d820649db021624a95cdcc77acf287f260a2
yurimaster2112 <104884614+yurimaster2112@users.noreply.github.com>
2023-11-05 00:14:17 -0400

    Update #2
    Nothing much

--- Model.py
+++ Model.py
@@ -1,8 +1,11 @@
 # Initial thought and codes to create model and define strategy
-
+# Option for strategies: Moving momentum, moving average
+  
 # Import
 import numpy as np
 import pandas as pd
+import math
+import xlsxwriter
 
 
 # Historical data needs to be in a csv format
