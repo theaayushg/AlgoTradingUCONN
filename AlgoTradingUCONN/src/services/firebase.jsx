40508e5b64b6839249babbe0706fada0ece45883
Ishan Tyagi <ishan.tyagi02@yahoo.com>
2023-11-06 22:03:18 -0500

    First Commit

---
+++ AlgoTradingUCONN/src/services/firebase.jsx
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfpTo6NTh41B0jx-0gov5ox7prSGQgmNA",
+  authDomain: "algotradinguconn.firebaseapp.com",
+  projectId: "algotradinguconn",
+  storageBucket: "algotradinguconn.appspot.com",
+  messagingSenderId: "839777988721",
+  appId: "1:839777988721:web:5d49a734b597a90a672bd0",
+  measurementId: "G-SB1L5D30Q0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//
+export const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
