d0aca8e52cbcaf4a41effe4c8c1c111abe8e9b7c
Chamomiletea2000 <130716165+Chamomiletea2000@users.noreply.github.com>
2023-12-05 03:29:14 +0000

    Now it should work theoretically, I only have 3 companies for now.

--- AlgoTradingUCONN/src/components/Graph.jsx
+++ AlgoTradingUCONN/src/components/Graph.jsx
@@ -1,30 +1,69 @@
 import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
+import { getApp } from 'firebase/app';
 import { drawGraph } from '../services/DataVisualization/dist/graph'; // Assuming this is your drawing logic
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 const Graph = () => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const canvas = d3.select(canvasRef.current);
-    const context = canvas.node().getContext('2d');
+    // Define your margins here
+  const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+  const width = 400 - margin.left - margin.right;
+  const height = 200 - margin.top - margin.bottom;
 
-    if (context) {
-      const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-      const width = 800 - margin.left - margin.right;
-      const height = 600 - margin.top - margin.bottom;
+  //comment out if you have it: 
+  const app = getApp();
+  const db = getFirestore(app);
 
-      const getRandomNumber = (min, max) => Math.random() * (max - min) + min;
-      const data = Array.from({ length: 100 }, () => ({
-        x: getRandomNumber(margin.left, width - margin.right),
-        y: getRandomNumber(margin.top, height - margin.bottom)
-      }));
+  async function fetchData() {
+      const querySnapshot = await getDocs(collection(db,"stocks"));
+      const data= [];
+      querySnapshot.forEach((doc) => {
+          data.push(doc.data());
+          console.log(doc.id, " => ", doc.data());
+      });
+      return data;
+  }
 
-      drawGraph(context, data, width, height, margin);
-    }
+  const companies = ["GOOGL", "APPL", "F"]; //should get this from firestore by running for loop to get all companies.
+  const select = d3.select("#companySelect");
+
+  select.selectAll("option")
+      .data(companies)
+      .enter()
+      .append("option")
+      .text(d =>d);
+
+  select.on("change",function() {
+      const company = d3.select(this).property("value");
+      fetchData(company).then(data => {
+          renderChart(data);
+      })});
+
+  function renderChart(data) {
+      //clear chart
+      d3.select("#chart").html("");
+          
+      // Set up svg
+      const svg = d3.select("#chart").append("svg")
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        // Call the drawGraph function with all necessary parameters
+        drawGraph(svg, data, width, height, margin);
+  }
+
+  // Fetch initial data and render chart
+  fetchData().then(data => {
+    renderChart(data);
+  });
   }, []); // Run this effect only once when the component mounts
 
-  return <canvas ref={canvasRef} width="800" height="600" />;
+  return <canvas ref={canvasRef} width="400" height="200" />;
 };
 
 export default Graph;
