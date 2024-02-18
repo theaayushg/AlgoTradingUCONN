2ebdb3dced4e8d1fd0f227f157dd425df894ebcc
theaayushg <aayush.gupta@uconn.edu>
2024-02-18 13:18:17 -0500

    Added routing for login-logout pages

--- AlgoTradingUCONN/src/components/Logout.jsx
+++ AlgoTradingUCONN/src/components/Logout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { auth } from '../services/firebase'
+import { useNavigate } from 'react-router-dom';
 import "../styles/Logout.css"
 
 const Logout = () => {
---
+++ AlgoTradingUCONN/src/components/SignInPage.jsx
@@ -0,0 +1,16 @@
+// SignInPage.jsx
+import React from 'react';
+import { signInWithGoogle } from '../services/firebase';
+import '../styles/SignInPage.css'; // You can create a new CSS file for styling this page
+import '../App.css';
+
+const SignInPage = () => {
+  return (
+    <div className="signin-page">
+      <h1>Welcome to Algo Trading Simulator</h1>
+      <button className="button" onClick={signInWithGoogle}>Sign in with Google</button>
+    </div>
+  );
+};
+
+export default SignInPage;
