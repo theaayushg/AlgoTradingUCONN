1b104fe333d620b55182bb60fdcb558690b19f7d
theaayushg <aayush.gupta@uconn.edu>
2024-03-25 17:22:45 -0400

    fixed logout button

--- AlgoTradingUCONN/src/components/SignInPage.jsx
+++ AlgoTradingUCONN/src/components/SignInPage.jsx
@@ -7,7 +7,7 @@ import '../App.css';
 const SignInPage = () => {
   return (
     <div className="signin-page">
-      <h1>Welcome to Algo Trading Simulator</h1>
+      <h1>Welcome To AlgoTradingUCONN!</h1>
       <button onClick={signInWithGoogle}>Sign in with Google</button>
     </div>
   );
