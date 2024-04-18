0742d4cb80d12cc77823764663a98b47824e38bb
Siddharth Sinha <siddharth.2.sinha@uconn.edu>
2024-04-17 20:13:09 -0400

    Fixed the about page

--- AlgoTradingUCONN/src/components/AboutUs.jsx
+++ AlgoTradingUCONN/src/components/AboutUs.jsx
@@ -3,6 +3,7 @@ import '../styles/AboutUs.css'; // Make sure to create this CSS file in the same
 
 function AboutUs() {
     const teamMembers = [
+        { name: 'Siddharth', role: 'CS Senior - Computational Data Analytics', description: 'Siddharth', image: '../../src/styles/Siddharth.jpg' },
         { name: 'Aayush', role: 'CS Senior - Computational Data Analytics', description: 'Aayush was the Team Manager for this project and focused on the web development along with research for this project.', image: '../../src/styles/aayush.PNG' },
         { name: 'Long', role: 'CS Senior - Cyber Security', description: 'Long.', image: '../../src/styles/long.PNG' },
         { name: 'Chenghe', role: 'CS Senior - Unspecialized', description: 'Chenghe.', image: '../../src/styles/chenghe.PNG' },
@@ -15,7 +16,7 @@ function AboutUs() {
     <div className="about-us">
       {teamMembers.map(member => (
         <div key={member.name} className="member">
-          <img src={member.image} alt={member.name} />
+          <img src={member.image} alt={member.name} style={{ width: '250px', height: 'auto' }} />
           <div className="member-info">
             <h3>{member.name}</h3>
             <p className="role">{member.role}</p>
