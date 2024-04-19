f41b99540f8d9023c9f0ea9624b90a7f50d0234c
yurimaster2112 <yurimaster2112@gmail.com>
2024-04-19 16:14:40 -0400

    12 point graphs and news section

--- AlgoTradingUCONN/src/assets/get_data.py
+++ AlgoTradingUCONN/src/assets/get_data.py
@@ -1,44 +1,6 @@
 from datetime import date, timedelta
 import pandas as pd
 import yfinance as yf
-<<<<<<< HEAD
-import numpy as np
-# import pandas_ta
-
-def get_data(company):
-  end = date.today()-timedelta(days=1)
-  print(f"date:{end}")
-  stack = []
-  counter = 0
-  datas = []
-  while True:
-    previous_date = end - timedelta(days=1)
-    stock_data = yf.download(company,previous_date,end)
-    if not stock_data.empty:
-      stack.append(stock_data)
-      counter += 1
-    if counter == 21:
-      break;
-    end = end - timedelta(days=1)
-  while len(stack) != 0:
-    datas.append(stack.pop())
-  return pd.concat(datas)
-
-def download_stock_data(tickers):
-    start_date = date.today() - timedelta(days=9)
-    end_date = date.today()
-
-    # Loop through each ticker and download the data
-    for ticker in tickers:
-        data = yf.download(ticker, start=start_date, end=end_date)
-        filename = f"{ticker}_data.csv"
-        data.to_csv(filename)
-        print(f"Data for {ticker} saved to {filename}")
-
-companies = ['AAPL']
-
-download_stock_data(companies)
-=======
 
 def get_data(company):
     end = date.today()
@@ -70,4 +32,3 @@ def get_data(company):
 tickerlist = ['AAPL', 'MSFT', 'JNJ', 'PG', 'KO', 'XOM', 'WMT', 'IBM', 'GE', 'F', 'GOOGL', 'AMZN', 'META', 'TSLA', 'NFLX', 'INTC', 'AMD', 'NVDA', 'V', 'PYPL']
 for ticker in tickerlist:
     get_data(ticker)
->>>>>>> b8684022f36da891912cc5ba5f5809c55c82de2d
