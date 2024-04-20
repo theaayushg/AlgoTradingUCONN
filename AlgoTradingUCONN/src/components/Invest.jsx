fde4b7e85bc52b2e28a7521b09c768132d98b145
Stranger_666 <79552555+666TheStranger@users.noreply.github.com>
2024-04-20 00:57:59 +0000

    Added investment.css and modifed invest.jsx's return

--- AlgoTradingUCONN/src/components/Invest.jsx
+++ AlgoTradingUCONN/src/components/Invest.jsx
@@ -6,7 +6,8 @@ import { increaseBalance } from "../services/IncreaseBalance";
 import { TransactionList } from "./Orders";
 import { Timestamp } from "firebase/firestore"
 import ErrorMessage from "../services/ErrorMessage";
-import "../styles/Invest.css"
+import "../styles/investment.css";
+import "../styles/Account.css"
 
 function Invest({ user, stockData, user_portfolio, setPortfolio, balance, setBalance }) {
   const [selectedStock, setSelectedStock] = useState("");
@@ -79,79 +80,82 @@ function Invest({ user, stockData, user_portfolio, setPortfolio, balance, setBal
   };
 
   return (
-<div>
-  <div className="invest__page">
-    <h2>Invest</h2>
-    <div className="app__container">
-      <button
-        onClick={() => handleActionChange("buy")}
-        className={action === "buy" ? "active" : ""}
-      >
-        Buy
-      </button>
-      <button
-        onClick={() => handleActionChange("sell")}
-        className={action === "sell" ? "active" : ""}
-      >
-        Sell
-      </button>
+    <div className="investment-app">
+      <div className="account-container">
+      <div className="investment-header">
+        <h2>Invest</h2>
+      </div>
+      <div className="action-buttons">
+        <button
+          onClick={() => handleActionChange("buy")}
+          className={action === "buy" ? "active" : ""}
+        >
+          Buy
+        </button>
+        <button
+          onClick={() => handleActionChange("sell")}
+          className={action === "sell" ? "active" : ""}
+        >
+          Sell
+        </button>
+      </div>
+      {action === "buy" && (
+        <div className="investment-form">
+          <select
+            value={selectedStock}
+            onChange={(e) => setSelectedStock(e.target.value)}
+          >
+            <option value="">Select a stock</option>
+            {stockData.map((stock) => (
+              <option key={stock.name} value={stock.name}>
+                {stock.name}
+              </option>
+            ))}
+          </select>
+          <input
+            type="number"
+            value={numShares}
+            onChange={(e) => setNumShares(e.target.value)}
+            placeholder="Number of shares"
+          />
+          <button onClick={handleBuyStock}>Enter</button>
+        </div>
+      )}
+      {action === "sell" && (
+        <div className="investment-form">
+          <select
+            value={selectedStock}
+            onChange={(e) => setSelectedStock(e.target.value)}
+          >
+            <option value="">Select a stock</option>
+            {user_portfolio.map((stock) => (
+              <option key={stock.ticker} value={stock.ticker}>
+                {stock.ticker}
+              </option>
+            ))}
+          </select>
+          <input
+            type="number"
+            value={numShares}
+            onChange={(e) => setNumShares(e.target.value)}
+            placeholder="Number of shares"
+          />
+          <button onClick={handleSellStock}>Enter</button>
+        </div>
+      )}
+      <div className="error-message">
+        {successMessage && (
+          <ErrorMessage message={successMessage} onClose={() => setSuccessMessage(null)} />
+        )}
+        {errorMessage && (
+          <ErrorMessage message={errorMessage} onClose={() => setErrorMessage(null)} />
+        )}
+      </div>
+      <div className="transaction-list">
+        <TransactionList userId={user.uid}/>
+      </div>
+      </div>
     </div>
-    {action === "buy" && (
-      <div className="app__container">
-        <select
-          value={selectedStock}
-          onChange={(e) => setSelectedStock(e.target.value)}
-        >
-          <option value="">Select a stock</option>
-          {stockData.map((stock) => (
-            <option key={stock.name} value={stock.name}>
-              {stock.name}
-            </option>
-          ))}
-        </select>
-        <input
-          type="number"
-          value={numShares}
-          onChange={(e) => setNumShares(e.target.value)}
-        />
-        <button onClick={handleBuyStock}>Buy</button>
-      </div>
-    )}
-    {action === "sell" && (
-      <div className="app__container">
-        <select
-          value={selectedStock}
-          onChange={(e) => setSelectedStock(e.target.value)}
-        >
-          <option value="">Select a stock</option>
-          {user_portfolio.map((stock) => (
-            <option key={stock.ticker} value={stock.ticker}>
-              {stock.ticker}
-            </option>
-          ))}
-        </select>
-        <input
-          type="number"
-          value={numShares}
-          onChange={(e) => setNumShares(e.target.value)}
-        />
-        <button onClick={handleSellStock}>Sell</button>
-      </div>
-    )}
-  </div>
-  <div className="error_message">
-    {successMessage && (
-      <ErrorMessage message={successMessage} onClose={() => setSuccessMessage(null)} />
-    )}
-    {errorMessage && (
-      <ErrorMessage message={errorMessage} onClose={() => setErrorMessage(null)} />
-    )}
-  </div>
-  <div>
-    <TransactionList userId={user.uid}/>
-  </div>
-</div>
-
   );
 }
 
