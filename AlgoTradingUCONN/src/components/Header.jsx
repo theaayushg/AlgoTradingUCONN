744b774572c277fe7b57ff649255ce6dc3dcf437
theaayushg <aayush.gupta@uconn.edu>
2024-04-19 20:33:49 -0400

    Fixed header styling

--- AlgoTradingUCONN/src/components/Header.jsx
+++ AlgoTradingUCONN/src/components/Header.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/Connecticut_Huskies_logo.svg';
 import { DisplayUserInfo } from './DisplayUserInfo';
 import '../styles/Header.css';
 
 function Header({ user, balance, setBalance }) {
-  //const [currentBalance, setCurrentBalance] = useState(0);
 
   const handleBalanceChange = (newBalance) => {
     setBalance(newBalance);
@@ -15,24 +14,25 @@ function Header({ user, balance, setBalance }) {
     <div className="header__wrapper">
       {/* logo */}
       <div className="header__logo">
-        <Link to ="/">
-          <img src={Logo} />
+        <Link to="/">
+          <img src={Logo} alt="AlgoTradingUCONN Logo" />
         </Link>
       </div>
       {/* page title */}
-        <h1 className="header__title"> AlgoTradingUCONN</h1>
-      {/* menuitems */}
-      <div className="header__menuItems"> 
-        <Link to="/AboutUs">AboutUs</Link>
+      <h1 className="header__title">AlgoTradingUCONN</h1>
+      {/* menu items */}
+      <div className="header__menuItems">
+        <Link to="/AboutUs">About Us</Link>
         <Link to="/Learn">Learn</Link>
         <Link to="/Invest">Invest</Link>
         <Link to="/account">Account Management</Link>
       </div>
-      <div className="header_userInfo">
-        < DisplayUserInfo user={user} onBalanceChange={handleBalanceChange} balance={balance} setBalance={setBalance} />
+      {/* user info */}
+      <div className="header__userInfo">
+        <DisplayUserInfo user={user} onBalanceChange={handleBalanceChange} balance={balance} setBalance={setBalance} />
       </div>
     </div>
   )
 }
 
-export default Header
+export default Header;
