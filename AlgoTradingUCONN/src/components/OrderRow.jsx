b11a8aef2599f4af8b5cf04489f8ae4d470568af
Ishan Tyagi <ishan.tyagi02@yahoo.com>
2024-04-19 22:41:26 -0400

    pre vid

--- AlgoTradingUCONN/src/components/OrderRow.jsx
+++ AlgoTradingUCONN/src/components/OrderRow.jsx
@@ -8,13 +8,13 @@ const OrderRow = ({ order }) => {
     const { BuyPrice, SellPrice, Shares } = stockData;
 
     return (
-        <div className="row">
-            <div className="row__intro">
+        <div className="orderrow">
+            <div className="orderrow__intro">
                 <h1>{stockTicker}</h1>
                 <p>{orderType} Order</p>
             </div>
-            <div className="row__numbers">
-                <p className="row__price">Price: ${orderType === 'BUY' ? BuyPrice : SellPrice}</p>
+            <div className="orderrow__numbers">
+                <p className="orderrow__price">Price: ${orderType === 'BUY' ? BuyPrice : SellPrice}</p>
                 <p>Shares: {Shares}</p>
                 <p>Date: {timeStamp.toLocaleDateString()}</p> {/* Ensure timestamp is a Date object */}
             </div>
