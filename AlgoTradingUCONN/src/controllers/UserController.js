9178d79a99d7a5297a121973c3153a655cadb71e
Ron Zatkovsky <130716165+Chamomiletea2000@users.noreply.github.com>
2024-04-25 20:26:02 -0400

    Delete AlgoTradingUCONN/src/controllers directory

--- AlgoTradingUCONN/src/controllers/UserController.js
+++
@@ -1,8 +0,0 @@
-exports.UserController={
-    getUser:(req,res)=>{
-        return res.send(req.params.id).status(200)
-    },
-    registerUser: (req,res) => {
-        return res.send({user_registered:req.body.uer}).status(200)
-    }
-}
